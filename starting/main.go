21125c30449058ce68282fd04d781fbe1e033780
Nati Melkamu <natimelkemu@gmail.com>
2025-07-23 07:12:08 -0700

    first commit

---
+++ starting/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+//simple calculator
+
+func add(a int, b int) {
+
+}
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println("This is a simple Go program.")
+	fmt.Println("It prints me")
+	nat := `this is man of the match 
+	ho is gonna live for ever 
+	and try not to mess with him for real for he will be on ur ass `
+
+	fmt.Println(nat)
+
+}
